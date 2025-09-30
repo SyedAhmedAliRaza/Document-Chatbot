764a2b176bae938546a7dbe56d5191914649ba29
Syed Ahmed Ali Raza <ahmed.aliraza51214@gmail.com>
2025-10-01 02:12:25 +0500

    Update document_processor.py

--- utils/document_processor.py
+++ utils/document_processor.py
@@ -1,3 +1,4 @@
+%%writefile utils/document_processor.py
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
